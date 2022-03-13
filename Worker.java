8c569a525fd60cb39a79dae6a2256c3b7350c7b0
Wojciech Panfil <wojtek.panfil@wp.pl>
2022-03-13 12:14:02 +0100

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -3,17 +3,16 @@ package pl.edu.pg.student.testowy;
 
 public class Worker implements Runnable {
     int result = 0;
-    Resource_saver saver;
-    public Worker(Resource_saver saver) {
-        this.saver = saver;
-    }
-    public void GiveTask(int value){
-        this.result = value;
+    public Worker(int saver) {
+        this.result = saver;
     }
     @Override
     public void run() {
         result *= 2;
-        //Epic computations.
-        saver.put(result);
+        //Epic computations;
+    }
+
+    public int getResult() {
+        return result;
     }
 }
